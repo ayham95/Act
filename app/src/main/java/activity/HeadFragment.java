b19cb61c26b2c40b07ce93df0574ec22a1190e27
ayham95 <orfali.ayham@gmail.com>
2015-09-02 05:01:52 +0300

    Action button not working yet

---
+++ app/src/main/java/activity/HeadFragment.java
@@ -0,0 +1,58 @@
+package activity;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.team.act.R;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HeadFragment extends Fragment {
+
+    TextView dayOfWeek, monthOdYear, dayOfMonth, year;
+
+
+    public HeadFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_head, container, false);
+        final Calendar myCalander = Calendar.getInstance();
+
+        dayOfWeek = (TextView) rootView.findViewById(R.id.day_of_week);
+        monthOdYear = (TextView) rootView.findViewById(R.id.month_of_year);
+        dayOfMonth = (TextView) rootView.findViewById(R.id.day_of_month);
+        year = (TextView) rootView.findViewById(R.id.year);
+
+        String[] days = new String[]{" ", "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"};
+        String day = days[myCalander.get(Calendar.DAY_OF_WEEK)];
+        dayOfWeek.setText(day);
+
+        String[] months = new String[] {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST","SEPTEMBER" , "OCTOBER", "NOVEMBER"};
+        String month = months[myCalander.get(Calendar.MONTH)];
+
+        int dayNumber = myCalander.get(Calendar.DAY_OF_MONTH);
+        dayOfMonth.setText(new StringBuilder().append(dayNumber) + ",");
+
+        int yearNumber = myCalander.get(Calendar.YEAR);
+        year.setText(new StringBuilder().append(yearNumber));
+
+
+        return rootView;
+    }
+
+
+}
--- app/src/main/java/activity/HomeFragment.java
+++ app/src/main/java/activity/HomeFragment.java
@@ -6,14 +6,19 @@ package activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.software.shell.fab.ActionButton;
 import com.team.act.R;
 
 
 public class HomeFragment extends Fragment {
+    ActionButton fab;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -29,6 +34,27 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        fab = (ActionButton) rootView.findViewById(R.id.action_button);
+        fab.setButtonColor(getResources().getColor(R.color.colorPrimary));
+        fab.setShadowColor(getResources().getColor(R.color.fab_material_grey_500));
+        fab.setShadowRadius(5.0f);
+        fab.setImageResource(R.drawable.calendar);
+
+
+        Fragment headFragment = new HeadFragment();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container1, headFragment);
+        fragmentTransaction.commit();
+
+
+
+        //second fragment..
+        Fragment listFragment = new ListFragment();
+        FragmentManager fragmentManager1 = getFragmentManager();
+        FragmentTransaction fragmentTransaction1 = fragmentManager1.beginTransaction();
+        fragmentTransaction1.add(R.id.fragment_container2, listFragment);
+        fragmentTransaction1.commit();
 
 
         // Inflate the layout for this fragment
