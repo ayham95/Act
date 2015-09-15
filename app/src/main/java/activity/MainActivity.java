fc37ca13c7db9e986a11c8c1531970aa9cc3ecb1
AlaaShammaa <alaa.shammaa96@gmail.com>
2015-09-15 14:20:51 +0300

    Added Weather to the ActionBar!!

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -13,18 +13,46 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.location.Location;
+import android.location.LocationManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import com.survivingwithandroid.weather.lib.WeatherClient; import com.survivingwithandroid.weather.lib.WeatherConfig;
+import com.survivingwithandroid.weather.lib.client.okhttp.WeatherDefaultClient;
+import com.survivingwithandroid.weather.lib.exception.WeatherLibException;
+import com.survivingwithandroid.weather.lib.model.CurrentWeather;
 
+import com.survivingwithandroid.weather.lib.provider.openweathermap.OpenweathermapProviderType;
+import com.survivingwithandroid.weather.lib.request.WeatherRequest;
 import com.software.shell.fab.ActionButton;
 import com.team.act.R;
 
 import java.util.Calendar;
 import java.util.Locale;
 
+import data.AppLocationService;
+
 public class MainActivity extends ActionBarActivity implements FragmentDrawer.FragmentDrawerListener {
-
+    float currentTemp;
     private static String TAG = MainActivity.class.getSimpleName();
-
+    AppLocationService appLocationService;
+    double latitude,longitude;
     private Toolbar mToolbar;
     private FragmentDrawer drawerFragment;
 
@@ -34,7 +62,6 @@ public class MainActivity extends ActionBarActivity implements FragmentDrawer.Fr
         setContentView(R.layout.activity_main);
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -43,7 +70,42 @@ public class MainActivity extends ActionBarActivity implements FragmentDrawer.Fr
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
         drawerFragment.setDrawerListener(this);
+        //Getting device latitude and longitude (Location)
+        appLocationService = new AppLocationService(MainActivity.this);
+        Location gpsLocation = appLocationService.getLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            latitude = gpsLocation.getLatitude();
+            longitude = gpsLocation.getLongitude();
+        }
+        else {
+            //Allowing the user to activate Location Services if it's disabled
+            showSettingsAlert();
+        }
 
+
+        //Getting Weather data
+        try {
+            WeatherClient client = (new WeatherClient.ClientBuilder()).attach(this)
+                    .httpClient(WeatherDefaultClient.class)
+                    .provider(new OpenweathermapProviderType())
+                    .config(new WeatherConfig())
+                    .build();
+            client.getCurrentCondition(new WeatherRequest(latitude,longitude), new WeatherClient.WeatherEventListener() {
+                @Override
+                public void onWeatherRetrieved(CurrentWeather currentWeather) {
+                    currentTemp = currentWeather.weather.temperature.getTemp();
+                    Log.d("WL", "City [" + currentWeather.weather.location.getCity() + "] Current temp [" + currentTemp + "]");
+                }
+                @Override
+                public void onWeatherError(WeatherLibException e) {
+                    Log.d("WL", "Weather Error - parsing data");
+                    e.printStackTrace(); }
+
+                @Override
+                public void onConnectionError(Throwable throwable) {
+                    Log.d("WL", "Connection error"); throwable.printStackTrace(); } });
+        } catch (Throwable t) {
+            t.printStackTrace(); }
 
 
         // display the first navigation drawer view on app launch
@@ -59,6 +121,10 @@ public class MainActivity extends ActionBarActivity implements FragmentDrawer.Fr
         View customView = layoutInflater.inflate(R.layout.custom_actionbar, null);
         TextView textView = (TextView) customView.findViewById(R.id.date);
         textView.setText(date);
+        //add weather to the action bar
+        final TextView weather = (TextView) customView.findViewById(R.id.weather);
+        weather.setText(Float.toString(currentTemp) + (char) 0x00B0);
+        //add the custom action bar layout
         mToolbar.addView(customView);
     }
 
@@ -75,12 +141,6 @@ public class MainActivity extends ActionBarActivity implements FragmentDrawer.Fr
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -112,5 +172,26 @@ public class MainActivity extends ActionBarActivity implements FragmentDrawer.Fr
             fragmentTransaction.replace(R.id.container_body, fragment);
             fragmentTransaction.commit();
         }
+    }
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+                MainActivity.this);
+        alertDialog.setTitle("SETTINGS");
+        alertDialog.setMessage("Enable Location Provider! Go to settings menu?");
+        alertDialog.setPositiveButton("Settings",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(
+                                Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        MainActivity.this.startActivity(intent);
+                    }
+                });
+        alertDialog.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        alertDialog.show();
     }
 }
